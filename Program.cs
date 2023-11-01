b315e373833a6abc121a5ea7edcdd39913dcbf5f
3mkkazemi <149534612+3mkkazemi@users.noreply.github.com>
2023-11-01 13:44:09 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            //mohammad mahdi mohammad kazemi
+            //تکلیف شماره6
+            //پیشرفته1
+            Console.WriteLine("number");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                int mmk = (i * (i + 1)) / 2;
+                Console.WriteLine(mmk + "");
+
+            }
+            Console.WriteLine("ma mitavanim...");
+            Console.ReadKey();
+        }
+    }
+}
